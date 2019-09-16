d266d85bb3ca8eb358314db3571526755ed9ed1d
msfisourabhs <sourabhs@mindfiresolutions.com>
2019-09-16 15:11:37 +0530

    Added files and changes for deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wdigicrm.wsgi --log-file -
--- wdigicrm/settings.py
+++ wdigicrm/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,6 +84,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
